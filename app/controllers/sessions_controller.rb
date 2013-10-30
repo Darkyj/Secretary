c7796d359cef1a5fc21437be88a084683dec33cd
Josue <josh.rojas25@gmail.com>
2013-10-30 14:14:57 -0700

    Loging in without cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,29 +1,51 @@
 class SessionsController < ApplicationController
-	def new
-	end
+	# def new
+	# end
+
+	# def create
+	# 	if user = User.authenticate(params[:email_address], params[:password])
+	# 		session[:current_user_id] = user.id
+	# 		redirect_to root_url
+	# 	end
+	# 	# Use the stuff listed below for cookie handling
+	# 	# user = User.find_by(email_address: params[:session][:email_address].downcase)
+	# 	# if user && user.authenticate(params[:session][:password])
+	# 	# 	sign_in user
+	# 	# 	redirect_to user
+	# 	# else
+	# 	# 	flash.now[:error] = "Invalid email/password combination."
+	# 	# 	render :new
+	# 	# end
+	# end
+
+	# def destroy
+	# 	@_current_user = session[:current_user_id] = nil
+	# 	redirect_to root_url, alert: "YOu have successfully logged out."
+	# 	# The below is for the cookie handling
+	# 	# sign_out
+	# 	# redirect_to signin_path
+	# end
 
 	def create
-		# user = User.authenticate(params[:session][:email_address], params[:session][:password])
-		user = User.find_by(email_address: params[:session][:email_address].downcase)
-		# if user.nil?
-		# 	flash.now[:error] = "Invalid email/password combination."
-		# 	render :new
-		# else
-		# 	sign_in user
-		# 	redirect_to user
-		# end
-		if user && user.authenticate(params[:session][:password])
-			# sign_in user
-			
-			redirect_to user
-		else
-			flash.now[:error] = "Invalid email/password combination."
-			render :new
-		end
-	end
+  		user = User.find_by(email_address: params[:user][:email]).try(:authenticate, params[:user][:password])
+  		if user == false || user.nil?
+  			redirect_to root_path, alert: "You've been logged in."#, flash: { message: "Incorrect email/password" }
+  		else
+  			sign_in(user)
+  			redirect_to user_path(user)
+  		end
+  	end
 
-	def destroy
-		sign_out
-		redirect_to signin_path
-	end
+  	def new
+    	if signed_in?
+      		redirect_to root_path
+    	else
+     		@user = User.new
+    	end
+  	end
+
+  	def destroy
+  		sign_out
+  		redirect_to root_path
+  	end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,17 +1,55 @@
 module SessionsHelper
-	def sign_in(user)
-		remember_token = User.new_remember_token
-		cookies.permanent[:remember_token] = remember_token
-		user.update_attribute(:remember_token, User.encrypt(remember_token))
-		self.current_user = user
+	# def sign_in(user)
+	# 	#Remember token as a cookie and then find the user record in the database
+	# 	# as the user moves from page to page
+	# 	remember_token = User.new_remember_token #create token
+	# 	cookies.permanent[:remember_token] = remember_token #remember the token for 20 years
+	# 	user.update_attribute(:remember_token, User.encrypt(remember_token)) #save the newly encrypted token to the database
+	# 	self.current_user = user #sets current user to the current user
+	# end
+
+	# def signed_in?
+	# 	!current_user.nil?
+	# end
+
+	# #stores users for later use
+	# def current_user=(user)
+	# 	@current_user = user
+	# end
+
+	# # set the @current_user instance to the user corresponding to the remember token if not currently defined
+	# def current_user
+	# 	remember_token = User.encrypt(cookies[:remember_token])
+	# 	@current_user ||= User.find_by(remember_token: remember_token)
+	# end
+
+
+	def sign_in user
+		session[:user_id] = user.id
 	end
 
-	def current_user=(user)
+	def current_user= user
 		@current_user = user
 	end
 
 	def current_user
-		remember_token = User.encrypt(cookies[:remember_token])
-		@current_user ||= User.find_by(remember_token: remember_token)
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
+
+	def signed_in?
+		!current_user.nil?
+	end
+
+	def sign_out
+		session[:user_id] = nil
+		self.current_user = nil
+	end
+
+	def current_user? user
+		user === current_user
+	end
+
+	def deny_access
+		redirect_to new_user_path, notice: "Please sign in to access this page."
 	end
 end
