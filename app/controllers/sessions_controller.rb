8f5a3c191bf13a0b21393c718a30ca7d8f97eaa9
Josue <josh.rojas25@gmail.com>
2013-10-30 14:46:25 -0700

    Fixes for signing in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,26 +26,27 @@ class SessionsController < ApplicationController
 	# 	# redirect_to signin_path
 	# end
 
-	def create
-  		user = User.find_by(email_address: params[:user][:email]).try(:authenticate, params[:user][:password])
-  		if user == false || user.nil?
-  			redirect_to root_path, alert: "You've been logged in."#, flash: { message: "Incorrect email/password" }
-  		else
-  			sign_in(user)
-  			redirect_to user_path(user)
-  		end
+  def create
+  	user = User.find_by(email_address: params[:user][:email]).try(:authenticate, params[:user][:password])
+  	if user == false || user.nil?
+  		redirect_to new_session_path, flash: { message: "Incorrect email/password" }
+  	else
+  		sign_in(user)
+  		redirect_to user_path(user)
   	end
+  end
 
-  	def new
-    	if signed_in?
-      		redirect_to root_path
-    	else
-     		@user = User.new
-    	end
-  	end
+  def new
+    if signed_in?
+      redirect_to root_path
+    else
+     @user = User.new
+    end
+  end
 
-  	def destroy
-  		sign_out
-  		redirect_to root_path
-  	end
+  def destroy
+  	sign_out
+  	redirect_to root_path
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ActiveRecord::Base
 	validates :first_name, :last_name, presence: true, length: { in: 2..20 }
 	validates :email_address, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
 	validates :phone_number, presence: true, uniqueness: true, numericality: { only_integer: true }
+	validates :password, presence: true, length: { minimum: 6 }, on: :create
+
+
+
 	before_save do
 		self.email_address.downcase!
 	end
